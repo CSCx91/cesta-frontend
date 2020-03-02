f7c6d112c61131be2f96227d03ef4b59c6b4368c
swornimbarahi <swornimbarahi@gmail.com>
2020-03-02 13:10:20 -0500

    Added Test component and route for visual checking

---
+++ src/components/TestComponent/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Import your testing components here!
+// Feel free to remove any test components that were already here
+
+const TestComponent = () => {
+	return (
+		<>
+			<Foo />
+		</>
+	);
+};
+
+const Foo = () => {
+	return <div>Foo</div>;
+};
+
+export default TestComponent;
