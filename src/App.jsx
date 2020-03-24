e1e193ec668b95f7c54ec3c1ec8ee92062437180
gonzen2310 <gonzaloreyesllauca@gmail.com>
2020-03-24 14:14:51 -0500

    Finished list of cards in dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import TestComponent from './components/TestComponent';
+// import TestComponent from './components/TestComponent';
 import './App.scss';
+// import Card from './components/Card';
+import CardList from './components/CardList';
 
 function App() {
 	return (
 		<div className='App'>
 			<Router>
 				<Switch>
-					<Route path='/test' component={TestComponent} />
+					<Route path='/test' component={CardList} />
 				</Switch>
 			</Router>
 		</div>
