0b29f500cbe59881b484546811fce5522eb48e6d
swornimbarahi <swornimbarahi@gmail.com>
2020-05-04 23:02:09 -0400

    added transaction type selector

--- src/components/TestComponent/index.jsx
+++ src/components/TestComponent/index.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 // Import your testing components here!
 // Feel free to remove any test components that were already here
 import Navbar from '../Navbar';
+import TransactionTypeSelector from '../TransactionTypeSelector';
 
 const TestComponent = () => {
 	return (
 		<>
 			<Navbar />
+			<TransactionTypeSelector />
 		</>
 	);
 };
